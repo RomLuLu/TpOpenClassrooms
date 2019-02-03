ec2fc4f5bb48a5f6b7e1c891b5203f35fa237e34
RomLuLu <romain.lulu@gmail.com>
2019-02-03 14:14:00 +0100

    Ajout du dossier contenant le tp ZCasino

---
+++ ZCasino/ZCasino.py
@@ -0,0 +1,10 @@
+"""
+Création du jeu ZCasino.
+C'est une roulette ou le joueur mise une somme d'argent sur des numéros allant de 0 à 49
+-> les N°pairs sont noirs et les N°impairs sont rouges.
+règles :
+    ->si le joueur mise sur le même N° que celui de la roulettre gains = 3 X mise plus la mise
+    ->si mauvais N° mais bonne couleur : gains = mise + 50%mise
+    -> sinon le joueur perd tout
+"""
+
