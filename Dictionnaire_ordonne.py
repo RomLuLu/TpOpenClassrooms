e39c723245b1d2f862f77b55b9642a2a6d80cb89
RomLuLu <romain.lulu@gmail.com>
2019-03-06 08:44:45 +0100

    modification des methodes add et sort
    Signed-off-by: RomLuLu <romain.lulu@gmail.com>

--- Dictionnaire_ordonne.py
+++ Dictionnaire_ordonne.py
@@ -1,14 +1,15 @@
-#Tp OC dictionnaire ordonné.
+# Tp OC dictionnaire ordonné.
 
-class DictionnaireOrdonne(object):
+
+class DictionnaireOrdonne:
     """Classe qui créera des dictionnaires ordonnés"""
 
-    def __init__(self,dico = {}, **kwargs):
+    def __init__(self, dico={}, **kwargs):
         self._cle = []
         self._val = []
-        
-        #Ajout suite à la lecture du corrigé proposé.
-        if not isinstance(dico,(dict, DictionnaireOrdonne)):
+
+        # Ajout suite à la lecture du corrigé proposé.
+        if not isinstance(dico, (dict, DictionnaireOrdonne)):
             raise TypeError("Le type attendu est soit un dictionnaire(usuel ou ordonné)")
 
         # quelque soit le type d'objet entré dans le constructeur,
@@ -32,62 +33,50 @@ class DictionnaireOrdonne(object):
         chaine += '}'
         return chaine
     # ajout suite à la correction (avant je n'utilisais que str pas repr. J'ai donc inversé)
+
     def __str__(self):
         return repr(self)
 
     def __getitem__(self, item):
         """Fonction d'affichage de la valeur"""
-        if item in self._cle :
+        if item in self._cle:
             i = self._cle.index(item)
             return self._val[i]
-        else :
+        else:
             return None
-
 
     def __setitem__(self, item, n_valeur):
         """Foncion qui creer une clé et qui ajoute/modifie sa valeur"""
         if item not in self._cle:
             self._cle.append(item)
             self._val.append(n_valeur)
-        else :
+        else:
             i = self._cle.index(item)
             self._val[i] = n_valeur
-
 
     def __delitem__(self, item):
         if item in self._cle:
             i = self._cle.index(item)
             del self._cle[i]
             del self._val[i]
-        else :
-            #Ajout après lecture du corrigé.
+        else:
+            # Ajout après lecture du corrigé.
             raise KeyError(" le clé : {0} ne se trouve pas dans le dictionnaire".format(item))
 
     def __contains__(self, item):
-        if item in self._cle :
+        if item in self._cle:
             return True
-        else :
+        else:
             return False
-
 
     def __len__(self):
         return len(self._cle)
-
-    def sort(self, reverse = False):
-        """Methode qui tri notre dictionnaire"""
-        if reverse == False :
-            self._cle.sort()
-            self._val.sort()            
-        elif reverse == True:
-            self._cle.sort(reverse = True) 
-            self._val.sort(reverse = True)
-
 
     def keys(self):
         """generateur de cle"""
         for item in self._cle:
             yield item
-    
+
     def values(self):
         """generateur de valeur"""
         for item in self._val:
@@ -99,8 +88,32 @@ class DictionnaireOrdonne(object):
             yield (self._cle[i], self._val[i])
 
     def __add__(self, other):
-        self._cle = self._cle + other._cle
-        self._val = self._val + other._cle
-        return self
+        dico = DictionnaireOrdonne()
+        dico._cle = self._cle
+        dico._val = self._val
+        for y, key_dico in enumerate(dico._cle):
+            for i, key_other in enumerate(other._cle):
+                if key_other == key_dico:
+                    dico._val[y] = other._val[i]
+                    other._cle.pop(i)
+                    other._val.pop(i)
+        dico._cle = dico._cle + other._cle
+        dico._val = dico._val + other._val
+        return dico
+
+    def sort(self, reverse=False):
+        """Methode qui tri notre dictionnaire"""
+        dico = DictionnaireOrdonne()
+        dico._val = sorted(self._val, reverse=reverse)
+        for val in dico._val:
+            for i, val_self in enumerate(self._val):
+                if val == val_self:
+                    dico._cle .append(self._cle[i])
+        return dico
 
 
+
+if __name__ == "__main__":
+
+    d1 = DictionnaireOrdonne(cle1=1, cle10=10, cle7=7, cle6=6)
+    print(d1.sort(reverse=True))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+other._cle.pop(i)
+                # other._val.pop(i)
